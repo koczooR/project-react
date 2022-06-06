b2168342611f1dcde347e8a0b6fd19abb0910bd9
koczooR <koczorowski94@gmail.com>
2022-06-06 11:57:06 +0200

    bg draw, player draw, movement

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,11 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 
+const playerDown = new Image();
+
 export const Canvas = () => {
   const canvasRef = useRef(null);
 
-  const [, setBackground] = useState(null);
   const [posX, setPosX] = useState(window.innerWidth / 2 - 4620 / 2 + 66);
   const [posY, setPosY] = useState(window.innerHeight / 2 - 2640 / 2);
+  const [playerDirection, setPlayerDirection] = useState(playerDown);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -17,36 +19,64 @@ export const Canvas = () => {
     const img = new Image();
     img.src = require("./assets/img/myProjectMap.png");
 
+    // const playerDown = new Image();
+    playerDown.src = require("./assets/img/playerDown.png");
+
+    const playerUp = new Image();
+    playerUp.src = require("./assets/img/playerUp.png");
+
+    const playerLeft = new Image();
+    playerLeft.src = require("./assets/img/playerLeft.png");
+
+    const playerRight = new Image();
+    playerRight.src = require("./assets/img/playerRight.png");
+
+    ctx.drawImage(img, posX, posY);
+
+    ctx.drawImage(
+      playerDirection,
+      0,
+      0,
+      192 / 4,
+      68,
+      window.innerWidth / 2 - 22,
+      window.innerHeight / 2,
+      192 / 4,
+      68
+    );
+
     const handleKeyDown = (e) => {
       if (e.key === "w") {
+        setPlayerDirection(playerUp);
         setPosY((prevState) => prevState + 8);
       }
 
       if (e.key === "s") {
+        setPlayerDirection(playerDown);
         setPosY((prevState) => prevState - 8);
       }
 
       if (e.key === "a") {
+        setPlayerDirection(playerLeft);
         setPosX((prevState) => prevState + 8);
       }
 
       if (e.key === "d") {
+        setPlayerDirection(playerRight);
         setPosX((prevState) => prevState - 8);
       }
     };
-
-    img.onload = () => {
-      ctx.drawImage(img, posX, posY);
-    };
-
-    setBackground(ctx.drawImage(img, posX, posY));
 
     document.addEventListener("keydown", handleKeyDown);
 
     return function cleanup() {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, [posX, posY]);
+  }, [posX, posY, playerDirection]);
 
-  return <canvas ref={canvasRef} />;
+  return (
+    <>
+      <canvas ref={canvasRef} />
+    </>
+  );
 };
