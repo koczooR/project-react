e0e5c7e84510843adfd84f11e71f7e10d2e51733
koczooR <koczorowski94@gmail.com>
2022-06-12 00:57:32 +0200

    npcMap, border, move animation

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,20 +1,29 @@
 import { useState, useEffect } from "react";
 import { useCanvas } from "./useCanvas";
+import { collisionsArray } from "./mapData";
+import {
+  img,
+  foreground,
+  foreground2,
+  playerDown,
+  playerUp,
+  playerLeft,
+  playerRight,
+  npcMap,
+} from "./images";
 
-const img = new Image();
-img.src = require("./assets/img/myProjectMap.png");
+const speed = 10;
 
-const playerDown = new Image();
-playerDown.src = require("./assets/img/playerDown.png");
+const totalFrames = 4;
+let currentFrame = 0;
+let framesDrawn = 0;
+let srcX = 0;
+let srcY = 0;
 
-const playerUp = new Image();
-playerUp.src = require("./assets/img/playerUp.png");
-
-const playerLeft = new Image();
-playerLeft.src = require("./assets/img/playerLeft.png");
-
-const playerRight = new Image();
-playerRight.src = require("./assets/img/playerRight.png");
+const playerMovement = () => {
+  currentFrame = currentFrame % totalFrames;
+  srcX = currentFrame * (192 / 4);
+};
 
 export const Canvas = () => {
   const defaultPosition = {
@@ -23,14 +32,24 @@ export const Canvas = () => {
   };
   const [position, setPosition] = useState({ ...defaultPosition });
   const [playerDirection, setPlayerDirection] = useState(playerDown);
+  const [npcPosition, setNpcPosition] = useState({
+    x: Math.floor(window.innerWidth / 2 - 192 / 4 + 716),
+    y: Math.floor(window.innerHeight / 2 - 66),
+  });
+
+  framesDrawn++;
+  if (framesDrawn >= 5) {
+    currentFrame++;
+    framesDrawn = 0;
+  }
 
   const canvasRef = useCanvas(([canvas, ctx]) => {
     ctx.drawImage(img, position.x, position.y);
 
     ctx.drawImage(
       playerDirection,
-      0,
-      0,
+      srcX,
+      srcY,
       192 / 4,
       68,
       canvas.width / 2 - 22,
@@ -38,106 +57,86 @@ export const Canvas = () => {
       192 / 4,
       68
     );
+
+    ctx.drawImage(
+      npcMap,
+      srcX,
+      srcY,
+      192 / 4,
+      68,
+      npcPosition.x,
+      npcPosition.y,
+      192 / 4,
+      68
+    );
+
+    ctx.drawImage(foreground, position.x, position.y);
+    ctx.drawImage(foreground2, position.x, position.y);
+
+    let borderMap = [];
+    for (let i = 0; i < collisionsArray.length; i += 70) {
+      borderMap.push(collisionsArray.slice(i, i + 70));
+    }
+
+    const borderElements = [];
+    borderMap.forEach((row, i) => {
+      row.forEach((element, j) => {
+        if (element === 1025) {
+          ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+          borderElements.push(
+            ctx.fillRect(j * 66 + position.x, i * 66 + position.y, 66, 66)
+          );
+        }
+      });
+    });
   });
 
+  const keyPress = (e) => {
+    if (e.key === "w") {
+      setPlayerDirection(playerUp);
+      setPosition((prevState) => ({ ...prevState, y: prevState.y + speed }));
+      playerMovement();
+      setNpcPosition((prevState) => ({
+        ...prevState,
+        y: prevState.y + speed,
+      }));
+    }
+
+    if (e.key === "s") {
+      setPlayerDirection(playerDown);
+      setPosition((prevState) => ({ ...prevState, y: prevState.y - speed }));
+      playerMovement();
+      setNpcPosition((prevState) => ({
+        ...prevState,
+        y: prevState.y - speed,
+      }));
+    }
+
+    if (e.key === "a") {
+      setPlayerDirection(playerLeft);
+      setPosition((prevState) => ({ ...prevState, x: prevState.x + speed }));
+      playerMovement();
+      setNpcPosition((prevState) => ({
+        ...prevState,
+        x: prevState.x + speed,
+      }));
+    }
+
+    if (e.key === "d") {
+      setPlayerDirection(playerRight);
+      setPosition((prevState) => ({ ...prevState, x: prevState.x - speed }));
+      playerMovement();
+      setNpcPosition((prevState) => ({
+        ...prevState,
+        x: prevState.x - speed,
+      }));
+    }
+  };
+
   useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === "w") {
-        setPlayerDirection(playerUp);
-        setPosition((prevState) => ({ ...prevState, y: prevState.y + 8 }));
-      }
-
-      if (e.key === "s") {
-        setPlayerDirection(playerDown);
-        setPosition((prevState) => ({ ...prevState, y: prevState.y - 8 }));
-      }
-
-      if (e.key === "a") {
-        setPlayerDirection(playerLeft);
-        setPosition((prevState) => ({ ...prevState, x: prevState.x + 8 }));
-      }
-
-      if (e.key === "d") {
-        setPlayerDirection(playerRight);
-        setPosition((prevState) => ({ ...prevState, x: prevState.x - 8 }));
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyDown);
-
-    return function cleanup() {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+    window.addEventListener("keydown", keyPress);
+    return () => window.removeEventListener("keydown", keyPress);
+  });
 
   return <canvas ref={canvasRef} />;
 };
-
-// export const Canvas = () => {
-//   const canvasRef = useRef();
-
-//   const defaultPosition = {
-//     x: Math.floor(window.innerWidth / 2 - 4620 / 2 + 66),
-//     y: Math.floor(window.innerHeight / 2 - 2640 / 2),
-//   };
-
-//   const [position, setPosition] = useState({ ...defaultPosition });
-//   const [playerDirection, setPlayerDirection] = useState(playerDown);
-
-//   useEffect(() => {
-//     const canvas = canvasRef.current;
-//     const ctx = canvas.getContext("2d");
-
-//     canvas.height = window.innerHeight;
-//     canvas.width = window.innerWidth;
-
-//     ctx.drawImage(img, position.x, position.y);
-
-//     ctx.drawImage(
-//       playerDirection,
-//       0,
-//       0,
-//       192 / 4,
-//       68,
-//       canvas.width / 2 - 22,
-//       canvas.height / 2,
-//       192 / 4,
-//       68
-//     );
-
-//     const handleKeyDown = (e) => {
-//       if (e.key === "w") {
-//         setPlayerDirection(playerUp);
-//         setPosition((prevState) => ({ ...prevState, y: prevState.y + 2 }));
-//         console.log(position.y);
-//       }
-
-//       if (e.key === "s") {
-//         setPlayerDirection(playerDown);
-//         setPosition((prevState) => ({ ...prevState, y: prevState.y - 2 }));
-//       }
-
-//       if (e.key === "a") {
-//         setPlayerDirection(playerLeft);
-//         setPosition((prevState) => ({ ...prevState, x: prevState.x + 2 }));
-//       }
-
-//       if (e.key === "d") {
-//         setPlayerDirection(playerRight);
-//         setPosition((prevState) => ({ ...prevState, x: prevState.x - 2 }));
-//       }
-//     };
-
-//     document.addEventListener("keydown", handleKeyDown);
-
-//     return function cleanup() {
-//       document.removeEventListener("keydown", handleKeyDown);
-//     };
-//   }, [position.x, position.y, playerDirection]);
-
-//   return (
-//     <>
-//       <canvas ref={canvasRef} />
-//     </>
-//   );
-// };
---
+++ src/images.js
@@ -0,0 +1,23 @@
+export const img = new Image();
+img.src = require("./assets/img/myProjectMap.png");
+
+export const foreground = new Image();
+foreground.src = require("./assets/img/Foreground.png");
+
+export const foreground2 = new Image();
+foreground2.src = require("./assets/img/Foreground2.png");
+
+export const playerDown = new Image();
+playerDown.src = require("./assets/img/playerDown.png");
+
+export const playerUp = new Image();
+playerUp.src = require("./assets/img/playerUp.png");
+
+export const playerLeft = new Image();
+playerLeft.src = require("./assets/img/playerLeft.png");
+
+export const playerRight = new Image();
+playerRight.src = require("./assets/img/playerRight.png");
+
+export const npcMap = new Image();
+npcMap.src = require("./assets/img/npcMap.png");
